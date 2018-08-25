b19505ddd67dcf23c625ac72972bb9a9f0f2514e
ililac <y17610889205@gmail.com>
2018-08-25 19:18:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <mheader></mheader>
+    <tab></tab>
+    <keep-alive>
+    <router-view></router-view></keep-alive>
+  </div>
+</template>
+
+<script>
+import Mheader from './components/m-header/m-header'
+import Tab from './components/tab/tab'
+export default {
+  name: 'App',
+  components: {
+    Mheader,
+    Tab
+  }
+}
+</script>
+
+<style lang="stylus">
+@import 'common/stylus/variable';
+
+#app {
+  color: $color-theme;
+}
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export const commonParams = {
+  g_tk: 1928093487,
+  notice: 0,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  format: 'jsonp'
+}
+export const options = {
+  param: 'jsonpCallback',
+  prefix: 'jp'
+}
+export const ERR_OK = 0
