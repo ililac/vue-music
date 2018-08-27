ab54c51de850822a37e5e4a7b5f5c45d2b2f71ab
ililac <y17610889205@gmail.com>
2018-08-27 14:03:23 +0800

    recomend、singer

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -1,11 +1,57 @@
 <template>
-    <div>
-
+    <div ref="wrapper">
+        <slot></slot>
     </div>
 </template>
-<script>
-export default {}
+<script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+export default {
+    props: {
+        probeType: {
+            type: Number,
+            default: 1
+        },
+        click: {
+            type: Boolean,
+            default: true
+        },
+        data: {
+            type: Array,
+            default: null
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this._initScroll()
+        }, 20)
+    },
+    methods: {
+        _initScroll() {
+            if (!this.$refs.wrapper) { return }
+            this.scroll = new BScroll(this.$refs.wrapper, {
+                probeType: this.probeType,
+                click: this.click
+            })
+        },
+        enable() {
+            this.scroll && this.scroll.enable()
+        },
+        disable() {
+            this.scroll && this.scroll.disable()
+        },
+        refresh() {
+            this.scroll && this.scroll.refresh()
+        }
+    },
+    watch: {
+        data() {
+            setTimeout(() => {
+                this.scroll.refresh()
+            }, 20)
+        }
+    }
+}
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus" scoped rel="stylesheet/stylus">
 </style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,138 +1,153 @@
 <template>
-  <div class="recommend" ref="recommend">
-    <div class="recommend-content">
-        <div>
-        <div v-if="recommends.length" class="slider-wrapper">
-            <slider>
-                <div v-for="(item, index) in recommends" :key="index">
-                    <a :href="item.linkUrl">
-                        <img :src="item.picUrl"/>
-                    </a>
+    <div class="recommend" ref="recommend">
+        <scroll ref="scroll" class="recommend-content" :data="distList">
+            <div>
+                <div v-if="recommends.length" class="slider-wrapper">
+                    <slider>
+                        <div v-for="(item, index) in recommends" :key="index">
+                            <a :href="item.linkUrl">
+                                <img class="needsclick" width="100%" @load="loadImage" ref="img" :src="item.picUrl" />
+                            </a>
+                        </div>
+                    </slider>
                 </div>
-            </slider>
-        </div>
-        <div class="recommend-list">
-            <h1 class="list-title">热门歌单推荐</h1>
-            <ul>
-                <li v-for="(item, index) in distList" class="item" :key="index">
-                    <div class="icon">
-                        <img :src="item.imgurl" width="60px" height="60px" alt="">
-                    </div>
-                    <div class="text">
-                        <h2 class="name" v-html="item.creator.name"></h2>
-                        <p class="desc" v-html="item.dissname"></p>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        </div>
+                <div class="recommend-list">
+                    <h1 class="list-title">热门歌单推荐</h1>
+                    <ul>
+                        <li v-for="(item, index) in distList" class="item" :key="index">
+                            <div class="icon">
+                                <img v-lazy="item.imgurl" width="60" height="60" alt="">
+                            </div>
+                            <div class="text">
+                                <h2 class="name" v-html="item.creator.name"></h2>
+                                <p class="desc" v-html="item.dissname"></p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="loading-container" v-show="!distList.length">
+                <loading></loading>
+            </div>
+        </scroll>
     </div>
-  </div>
 </template>
 
 <script type="text/ecmascript-6">
 import Slider from 'base/slider/slider'
 import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
+import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
 export default {
-  data() {
-    return {
-      recommends: [],
-      distList: []
-    }
-  },
-  created() {
-    this._getRecommend()
-    this._getDiscList()
-  },
-  methods: {
-    _getRecommend() {
-      getRecommend().then(res => {
-        if (res.code === ERR_OK) {
-          this.recommends = res.data.slider
+    data() {
+        return {
+            recommends: [],
+            distList: []
         }
-      })
     },
-    _getDiscList() {
-      getDiscList().then(res => {
-        if (res.code === ERR_OK) {
-          this.distList = res.data.list
+    created() {
+        this._getRecommend()
+        setTimeout(() => {
+            this._getDiscList()
+        }, 1000)
+    },
+    methods: {
+        _getRecommend() {
+            getRecommend().then(res => {
+                if (res.code === ERR_OK) {
+                    this.recommends = res.data.slider
+                }
+            })
+        },
+        _getDiscList() {
+            getDiscList().then(res => {
+                if (res.code === ERR_OK) {
+                    this.distList = res.data.list
+                }
+            })
+        },
+        loadImage() {
+            if (!this.checkLoaded) {
+                this.$refs.scroll.refresh()
+                this.checkLoaded = true
+            }
         }
-      })
+    },
+    components: {
+        Slider,
+        Scroll,
+        Loading
     }
-  },
-  components: {
-    Slider
-  }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import '~common/stylus/variable'
+@import '~common/stylus/variable';
 
 .recommend {
-    position: fixed
-    width: 100%
-    top: 88px
-    bottom: 0
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
 
     .recommend-content {
-        height: 100%
-        overflow: hidden
+        height: 100%;
+        overflow: hidden;
 
         .slider-wrapper {
-            position: relative
-            width: 100%
-            overflow: hidden
+            position: relative;
+            width: 100%;
+            overflow: hidden;
         }
 
         .recommend-list {
             .list-title {
-                height: 65px
-                line-height: 65px
-                text-align: center
-                font-size: $font-size-medium
-                color: $color-theme
+                height: 65px;
+                line-height: 65px;
+                text-align: center;
+                font-size: $font-size-medium;
+                color: $color-theme;
             }
 
             .item {
-                display: flex
-                box-sizing: border-box
-                align-items: center
-                padding: 0 20px 20px 20px
+                display: flex;
+                box-sizing: border-box;
+                align-items: center;
+                padding: 0 20px 20px 20px;
 
                 .icon {
-                    flex: 0 0 60px
-                    width: 60px
-                    padding-right: 20px
+                    flex: 0 0 60px;
+                    width: 60px;
+                    padding-right: 20px;
                 }
 
                 .text {
-                    display: flex
-                    flex-direction: column
-                    justify-content: center
-                    flex: 1
-                    line-height: 20px
-                    overflow: hidden
-                    font-size: $font-size-medium
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    flex: 1;
+                    line-height: 20px;
+                    overflow: hidden;
+                    font-size: $font-size-medium;
 
                     .name {
-                        margin-bottom: 10px
-                        color: $color-text
+                        margin-bottom: 10px;
+                        color: $color-text;
                     }
 
                     .desc {
-                        color: $color-text-d
+                        color: $color-text-d;
                     }
                 }
             }
         }
 
         .loading-container {
-            position: absolute
-            width: 100%
-            top: 50%
-            transform: translateY(-50%)
+            position: absolute;
+            width: 100%;
+            top: 50%;
+            transform: translateY(-50%);
         }
     }
 }
